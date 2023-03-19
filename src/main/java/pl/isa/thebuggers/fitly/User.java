1b32cacf50420860413fe6f651f0bad7ca3d83fe
Wojciech Walczak <wwalczakpraca@wp.pl>
2023-03-19 09:44:41 +0100

    first settings of UserData

--- src/main/java/pl/isa/thebuggers/fitly/User.java
+++ src/main/java/pl/isa/thebuggers/fitly/User.java
@@ -5,24 +5,42 @@ public class User {
     private String name;
     private int age;
     private int weight;
+    private boolean gender;
+    private int activity;
+/*----Dane Uzytkownika---- 1.Wzrost 2.Waga 3.Plec 4.Aktywnosc fiz.
+----Dane Wyjsciowe------ 1.BMI 2.Dieta Default 3.Trening Default
+----Funkcjonalnosci----- 1.Pobieranie danych uzytkownika 2.wyswietlanie bmi
+3.wyswietlanie diety default 4.wyswietlanie treningu default
+ */
 
 //zrobic warunki dla integerow i Stringow, żeby nie przyjmowały niewłasciwych wartości
 
     public String userName() {
-        this.name = name;
+
         return name;
     }
 
     public int userAge(){
-        this.age = age;
+
          return age;
     }
 
     public int userWeight(){
-        this.weight = weight;
+
 
         return weight;
 
     }
+//utworzyć mozliwość wyboru male/female
+    public boolean userGender() {
+        if{
+         else
+        }
+        return gender;
+    }
+//utworzyć skalę wyboru aktywności w zakładanym zakresie np. 1-5
+    public int userActivity() {
 
+        return activity;
+    }
 }
