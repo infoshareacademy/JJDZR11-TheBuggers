8548362a29a73d1e2734e0ad6f0370d3a20d5f6a
Wojciech Walczak <wwalczakpraca@wp.pl>
2023-03-29 19:59:59 +0200

    Main - sprawdza działanie klas: UserData i UserService UserData - pobiera dane użytkownika UserService - zapisuje dane do Json

--- src/main/java/pl/isa/thebuggers/fitly/Main.java
+++ src/main/java/pl/isa/thebuggers/fitly/Main.java
@@ -1,18 +1,9 @@
 package pl.isa.thebuggers.fitly;
 public class Main {
     public static void main(String[] args) {
-
-
 UserData userData = new UserData();
 UserService userService = new UserService();
-
         System.out.println(userService.json);
         System.out.println(userData.getAge());
-
-
-
-
-
-
     }
 }
--- src/main/java/pl/isa/thebuggers/fitly/UserService.java
+++ src/main/java/pl/isa/thebuggers/fitly/UserService.java
@@ -4,14 +4,9 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.internal.bind.util.ISO8601Utils;
 import java.nio.file.Path;
 import java.nio.file.Files;
-
 public class UserService extends UserData {
     Gson gson = new Gson();
     GsonBuilder gsonBuilder = new GsonBuilder();
     UserData userData = new UserData();
     String json = gson.toJson(userData);
-
-
-
-
 }
