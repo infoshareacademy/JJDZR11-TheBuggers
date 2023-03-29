9dc10b11933040a9042a76bbc4851215e05bf1e2
Wojciech Walczak <wwalczakpraca@wp.pl>
2023-03-29 19:57:59 +0200

    import Gson

--- src/main/java/pl/isa/thebuggers/fitly/Main.java
+++ src/main/java/pl/isa/thebuggers/fitly/Main.java
@@ -3,7 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
 
+UserData userData = new UserData();
+UserService userService = new UserService();
 
+        System.out.println(userService.json);
+        System.out.println(userData.getAge());
 
 
 
