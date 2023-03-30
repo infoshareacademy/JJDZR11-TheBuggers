99cb04bb34fd5968ec2128bfdf1efa5fcf04afed
Bartosz Mazur <mazurbartosz99@gmail.com>
2023-03-30 23:02:35 +0200

    Main: Funcional menu

--- src/main/java/pl/isa/thebuggers/fitly/Main.java
+++ src/main/java/pl/isa/thebuggers/fitly/Main.java
@@ -1,6 +1,42 @@
 package pl.isa.thebuggers.fitly;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Option 1");
+            System.out.println("2. Option 2");
+            System.out.println("3. Option 3");
+            System.out.println("0. Exit");
+            System.out.print("Choose an option: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("1");
+                    break;
+                case 2:
+                    System.out.println("2");
+                    break;
+                case 3:
+                    System.out.print("Enter your weight in kg: ");
+                    double weight = scanner.nextDouble();
+                    Training training = new Training(weight);
+                    training.calculateTraining();
+                    training.calculateTrainingWeightBasedOnRM();
+                    training.displayTrainingFile();
+                    break;
+                case 0:
+                    System.out.println("Exit");
+                    break;
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+        } while (choice != 0);
     }
 }
