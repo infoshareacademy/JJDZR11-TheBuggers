55e3d79d63b6e8b77676f3407d75616f58cf6a24
Wojciech Walczak <wwalczakpraca@wp.pl>
2023-04-16 20:02:24 +0200

    small bugs

--- src/main/java/pl/isa/thebuggers/fitly/Diet.java
+++ src/main/java/pl/isa/thebuggers/fitly/Diet.java
@@ -1,11 +1,9 @@
 package pl.isa.thebuggers.fitly;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
 public class Diet {
     private static final double MALE_BMR_CONSTANT = 88.362;
     private static final double MALE_BMR_WEIGHT_COEFFICIENT = 13.397;
@@ -17,8 +15,6 @@ public class Diet {
     private static final double FEMALE_BMR_AGE_COEFFICIENT = 4.330;
     private static final double BMR_MALE_ADJUSTMENT = 5;
     private static final double BMR_FEMALE_ADJUSTMENT = -161;
-
-
     public static class ActivityLevels {
         public static final String FIRST_LVL = "Sedentary";
         public static final String SECOND_LVL = "Lightly active";
@@ -26,7 +22,6 @@ public class Diet {
         public static final String FOURTH_LVL = "Very active";
         public static final String FIFTH_LVL = "Super active";
     }
-
     private static Map<String, Double> createActivityFactorsMap() {
         Map<String, Double> activityLevel = new HashMap<>();
         activityLevel.put(ActivityLevels.FIRST_LVL, 1.2);
@@ -36,19 +31,16 @@ public class Diet {
         activityLevel.put(ActivityLevels.FIFTH_LVL, 1.9);
         return activityLevel;
     }
-
     static double calculateBmrForMale(double weight, double height, int age) {
         return (MALE_BMR_CONSTANT + (MALE_BMR_WEIGHT_COEFFICIENT * weight)
                 + (MALE_BMR_HEIGHT_COEFFICIENT * height) - (MALE_BMR_AGE_COEFFICIENT * age))
                 + BMR_MALE_ADJUSTMENT;
     }
-
     static double calculateBmrForFemale(double weight, double height, int age) {
         return (FEMALE_BMR_CONSTANT + (FEMALE_BMR_WEIGHT_COEFFICIENT * weight)
                 + (FEMALE_BMR_HEIGHT_COEFFICIENT * height) - (FEMALE_BMR_AGE_COEFFICIENT * age))
                 + BMR_FEMALE_ADJUSTMENT;
     }
-
     static double getActivityFactor(String activityLevel) {
         Map<String, Double> activityLevelMap = createActivityFactorsMap();
         if (activityLevelMap.containsKey(activityLevel)) {
@@ -57,18 +49,12 @@ public class Diet {
             throw new IllegalArgumentException("Invalid activity level: " + activityLevel);
         }
     }
-
     private static final String[] DIET_FILES = {
             "1500kcl.txt", "1800kcl.txt", "2000kcl.txt", "2200kcl.txt", "2500kcl.txt",
             "2800kcl.txt", "3000kcl.txt", "3200kcl.txt", "3500kcl.txt", "3800kcl.txt", "4000kcl.txt"
     };
-
     static String selectAndDisplayDietFile(double dailyCalorieNeeds) {
         String selectedFile = null;
-//        final String[] DIET_FILES = {
-//                "1500kcl.txt", "1800kcl.txt", "2000kcl.txt", "2200kcl.txt", "2500kcl.txt",
-//                "2800kcl.txt", "3000kcl.txt", "3200kcl.txt", "3500kcl.txt", "3800kcl.txt", "4000kcl.txt"
-//        };
         for (String file : DIET_FILES) {
             String[] parts = file.split("kcl");
             int calorieLimit = Integer.parseInt(parts[0]);
@@ -80,9 +66,7 @@ public class Diet {
         if (selectedFile == null) {
             selectedFile = DIET_FILES[DIET_FILES.length - 1];
         }
-
         String nazwaPliku = "./diety/" + selectedFile;
-
         try (BufferedReader dietFileReader = new BufferedReader(new FileReader(nazwaPliku))) {
             String linia;
             String kclLevel = selectedFile.substring(0, selectedFile.indexOf('k'));
