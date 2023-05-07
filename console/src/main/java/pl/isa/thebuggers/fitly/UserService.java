a0f4f3a7cbe749a42d1e3b2706652f3657ed4cd5
Wojciech Walczak <wwalczakpraca@wp.pl>
2023-05-07 19:48:21 +0200

    front HTML initialized

--- console/src/main/java/pl/isa/thebuggers/fitly/UserService.java
+++ console/src/main/java/pl/isa/thebuggers/fitly/UserService.java
@@ -24,4 +24,5 @@ public class UserService {
         reader.close();
         return data;
     }
+
 }
--- web-fitly/src/main/java/pl/isa/fitly/FitlyApplication.java
+++ web-fitly/src/main/java/pl/isa/fitly/FitlyApplication.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FitlyApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(FitlyApplication.class, args);
 	}
+
+
+
 
 }
