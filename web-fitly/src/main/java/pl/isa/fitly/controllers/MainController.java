834c8e04648c1faa20b835c1f4138a9b0b9656df
Piotr Fiuk <piotrek.fiuk@gmail.com>
2023-05-16 21:20:14 +0200

    new TEST branch added - trying to fix Thymeleaf configuration

---
+++ web-fitly/src/main/java/pl/isa/fitly/controllers/MainController.java
@@ -0,0 +1,13 @@
+package pl.isa.fitly.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "main";
+    }
+}
