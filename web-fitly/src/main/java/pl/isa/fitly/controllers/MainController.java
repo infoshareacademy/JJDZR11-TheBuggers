4de2920e80c3b005dd987d08726185904c8f0dee
Piotr Fiuk <piotrek.fiuk@gmail.com>
2023-05-16 22:57:57 +0200

    added log4j2.xml file, changed dependencies and pom.xml file content - many times. Thymeleaf-related log messages are not present ISSUE

--- web-fitly/src/main/java/pl/isa/fitly/controllers/MainController.java
+++ web-fitly/src/main/java/pl/isa/fitly/controllers/MainController.java
@@ -1,10 +1,16 @@
 package pl.isa.fitly.controllers;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    @GetMapping("/main")
+    public String mainPage(Model model) {
+        // Add any necessary model attributes
+        return "main"; // Returns the template name without the extension
+    }
 
     @GetMapping("/")
     public String home() {
