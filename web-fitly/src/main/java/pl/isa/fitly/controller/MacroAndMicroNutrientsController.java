05be636821a794bec112d25691d158032735a1eb
Bartosz Mazur <mazurbartosz99@gmail.com>
2023-07-14 22:20:46 +0200

    MacroAndMicroNutrient - added without front

---
+++ web-fitly/src/main/java/pl/isa/fitly/controller/MacroAndMicroNutrientsController.java
@@ -0,0 +1,61 @@
+package pl.isa.fitly.controller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.isa.fitly.service.Product;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MacroAndMicroNutrientsController {
+    @GetMapping("/MacroAndMicroNutrients")
+    public String macroAndMicroNutrients() {
+        return "MacroAndMicroNutrients";
+    }
+
+    @PostMapping("/MacroAndMicroNutrients/search")
+    public String search(@RequestParam("productName") String productName, Model model) {
+        try {
+            // Wczytaj zawartość pliku JSON do łańcucha tekstowego
+            ClassPathResource resource = new ClassPathResource("foods.json");
+            byte[] jsonData = FileCopyUtils.copyToByteArray(resource.getInputStream());
+            String jsonStr = new String(jsonData, StandardCharsets.UTF_8);
+
+            // Inicjalizuj ObjectMapper
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // Parsuj dane JSON
+            List<Product> products = objectMapper.readValue(jsonStr, new TypeReference<List<Product>>() {
+            });
+
+            productName = productName.toLowerCase();
+
+            List<Product> foundProducts = new ArrayList<>();
+
+            // Przeszukaj dane JSON w poszukiwaniu pasujących produktów
+            for (Product product : products) {
+                String name = product.getName().toLowerCase();
+
+                if (name.contains(productName)) {
+                    foundProducts.add(product);
+                }
+            }
+
+            model.addAttribute("foundProducts", foundProducts);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "MacroAndMicroNutrients";
+    }
+}
---
+++ web-fitly/src/main/java/pl/isa/fitly/service/Product.java
@@ -0,0 +1,69 @@
+package pl.isa.fitly.service;
+
+import java.util.Map;
+
+public class Product {
+    private String name;
+    private int serving;
+    private Map<String, Double> nutrients;
+    private double fat;
+    private double calories;
+    private double proteins;
+    private double carbohydrates;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getServing() {
+        return serving;
+    }
+
+    public void setServing(int serving) {
+        this.serving = serving;
+    }
+
+    public Map<String, Double> getNutrients() {
+        return nutrients;
+    }
+
+    public void setNutrients(Map<String, Double> nutrients) {
+        this.nutrients = nutrients;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
+
+    public double getProteins() {
+        return proteins;
+    }
+
+    public void setProteins(double proteins) {
+        this.proteins = proteins;
+    }
+
+    public double getCarbohydrates() {
+        return carbohydrates;
+    }
+
+    public void setCarbohydrates(double carbohydrates) {
+        this.carbohydrates = carbohydrates;
+    }
+}
