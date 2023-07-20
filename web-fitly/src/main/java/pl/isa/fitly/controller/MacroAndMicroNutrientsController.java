4659b7eccd1040bc55ddbae65e5cad5e217db91e
Piotr Fiuk <piotrek.fiuk@gmail.com>
2023-07-20 15:27:29 +0200

    FRONTEND: micro / macro subpage ready

--- web-fitly/src/main/java/pl/isa/fitly/controller/MacroAndMicroNutrientsController.java
+++ web-fitly/src/main/java/pl/isa/fitly/controller/MacroAndMicroNutrientsController.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Controller
 public class MacroAndMicroNutrientsController {
     @GetMapping("/MacroAndMicroNutrients")
-    public String macroAndMicroNutrients() {
+    public String MacroAndMicroNutrients() {
         return "MacroAndMicroNutrients";
     }
 
